58769b598b182f0b09068bfd32cb5994e72c2c4f
Tom Flahive <TomPFlahive@gmail.com>
2023-03-21 06:43:58 -0700

    Initial Commit

---
+++ Test_InfoButton_ChatGPT4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test_InfoButton_ChatGPT4
+//
+//  Created by Tom Flahive on 3/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
